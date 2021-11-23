4a370e1739c0d67bcb869183253b440e575802cf
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-11-24 05:48:36 +0800

    佈局(主樣板)與視圖(子樣板)

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 //顯示所有任務的清單
 Route::get('/', function () {
-    //return view('welcome');
+    return view('tasks');
 });
 //增加新的任務
 Route::post('/task',function(\http\Env\Request $request){
