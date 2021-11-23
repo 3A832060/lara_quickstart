d9094bdbcdc5c838547fdaa87d277f5cb64f41ad
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-11-24 05:38:56 +0800

    初步建置三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//顯示所有任務的清單
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+});
+//增加新的任務
+Route::post('/task',function(\http\Env\Request $request){
+    //
+});
+//刪除任務
+Route::delete('/task/{task}',function(Task $task){
+    //
 });
